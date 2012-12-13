1607c295ff60fb82ef3bfa845bb447a4d2cb0235
andy <andy@whiskeymedia.com>
2012-12-12 16:41:55 -0800

    allow for GalleryItems without photos -- perhaps item data lives in app_data

--- ella_galleries/models.py
+++ ella_galleries/models.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.datastructures import SortedDict
 from django.core.cache import cache
+from django.core.validators import validate_slug
 
 from app_data import AppDataField
 
@@ -45,22 +47,17 @@ class Gallery(Publishable):
 
     @cache_this(get_gallery_key)
     def _get_gallery_items(self):
-        slugs_count = {}
-        itms = [(item, item.photo) for item in self.galleryitem_set.order_by('order')]
-        slugs_unique = set((i[1].slug for i in itms))
+        slugs_count = defaultdict(int)
+        slugs_unique = set()
         res = SortedDict()
 
-        for item, target in itms:
-            slug = target.slug
-            if slug not in slugs_count:
-                slugs_count[slug] = 1
-                res[slug] = item
-            else:
-                while "%s%s" % (slug, slugs_count[slug]) in slugs_unique:
-                    slugs_count[slug] += 1
-                new_slug = "%s%s" % (slug, slugs_count[slug])
-                slugs_unique.add(new_slug)
-                res[new_slug] = item
+        for item in self.galleryitem_set.order_by('order'):
+            slug = item.get_item_slug()
+            while slug in slugs_unique:
+                slugs_count[slug] += 1
+                slug = "%s%s" % (slug, slugs_count[slug])
+            slugs_unique.add(slug)
+            res[slug] = item
         return res
 
     def get_photo(self):
@@ -83,6 +80,8 @@ class GalleryItem(models.Model):
     ``title`` - specific title in the gallery, can be blank
     ``text`` - description of photo in the gallery, can be blank too
     """
+    slug = models.SlugField(_('Slug'), max_length=255,
+                            validators=[validate_slug], null=True)
     gallery = models.ForeignKey(Gallery, verbose_name=_("Parent gallery"))
     photo = CachedForeignKey(Photo, verbose_name=_("Photo"),
                              blank=True, null=True)
@@ -99,7 +98,7 @@ class GalleryItem(models.Model):
         verbose_name_plural = _('Gallery items')
 
     def __unicode__(self):
-        return u"%s %s %s" % (self.photo, _('in gallery'), self.gallery.title)
+        return u"%s %s %s" % (self.get_item_title(), _('in gallery'), self.gallery.title)
 
     def __get_slug(self):
         for slug, item in self.gallery.items.items():
@@ -114,6 +113,12 @@ class GalleryItem(models.Model):
         if not hasattr(self, '__slug'):
             self.__slug = self.__get_slug()
         return self.__slug
+
+    def get_item_slug(self):
+        return self.slug or (self.photo and self.photo.slug)
+
+    def get_item_title(self):
+        return self.title or (self.photo and self.photo.title)
 
     def get_absolute_url(self):
         if self.order == 0:
